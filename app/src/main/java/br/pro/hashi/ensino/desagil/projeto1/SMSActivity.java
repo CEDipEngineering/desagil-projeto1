a3e3deb05959143fef321dd064cb4fe1d520eb9b
jpgianfaldoni <jpgianfaldoni@gmail.com>
2020-05-09 17:42:19 -0300

    atualiza SMSActivity

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -11,9 +11,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.LinkedList;
+
 
 //Código baseado de ExemploSMS da matéria Desenvolvimento Colaborativo Ágil
 public class SMSActivity extends AppCompatActivity {
+    private static final Translator translator = new Translator(); //protected quando tiverem mais activities?
+    private final LinkedList<Character> outputChars = new LinkedList<>();
+    private TextView text;
+    private TextView textWord;
+    private TextView textNumber;
+    private TextView textPhone;
+    private TextView editText;
+    private TextView textMessage;
+    private Button buttonSend;
+    private Button buttonMorse;
+    private Button buttonDelete;
+    private Button buttonEndChar;
+    private Button homeButton;
+    private Button SMSButton;
     private Button backButton;
     // Método de conveniência para mostrar uma bolha de texto.
     private void showToast(String text) {
@@ -29,17 +45,59 @@ public class SMSActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_m_s);
-
-        TextView textMessage = findViewById(R.id.text_message);
-        EditText textPhone = findViewById(R.id.text_phone);
-        Button buttonSend = findViewById(R.id.button_send);
         Intent myIntent = getIntent();
         String message = myIntent.getStringExtra("message");
-        backButton = findViewById(R.id.home_button);
+
+        textMessage = findViewById(R.id.textWord);
+        textNumber = findViewById(R.id.textNumber);
+        buttonSend = findViewById(R.id.button_SMS);
+        backButton = findViewById(R.id.back_button);
+        text = findViewById(R.id.textLetter);
+        textWord = findViewById(R.id.textWord);
+        textNumber = findViewById(R.id.textNumber);
+        editText = findViewById(R.id.edit_text);
+        buttonMorse = findViewById(R.id.button_morse);
+        buttonDelete = findViewById(R.id.button_delete);
+        buttonEndChar = findViewById(R.id.button_endChar);
 
         this.backButton.setOnClickListener((view) -> {
             Intent intent=new Intent(SMSActivity.this, MainActivity.class);
             startActivity(intent);
+        });
+        // Write dot if short click;
+        this.buttonMorse.setOnClickListener((view) -> {
+            writeDot();
+            update();
+        });
+
+        // Write dash if long click;
+        this.buttonMorse.setOnLongClickListener((view) -> {
+            writeDash();
+            update();
+            // Has to return true to say that the longClick has been dealt with;
+            return true;
+        });
+
+        // Delete last signal, (or character if no signal present);
+        this.buttonDelete.setOnClickListener((view) -> {
+            deleteMorse();
+            update();
+
+
+        });
+
+        // Clear everything if long click on delete button;
+        this.buttonDelete.setOnLongClickListener((view) -> {
+            clearMsg();
+            update();
+            // Has to return true to say that the longClick has been dealt with;
+            return true;
+        });
+
+        // End character, (or write space if no signal present);
+        this.buttonEndChar.setOnClickListener((view) -> {
+            endChar();
+            update();
         });
 
         textMessage.setText(message);
@@ -70,4 +128,65 @@ public class SMSActivity extends AppCompatActivity {
             textPhone.setText("");
         });
     }
+    private void update() {
+
+        // Try to translate editText
+        String morse = editText.getText().toString();
+        String character = "Letra: ";
+        if (translator.morseToChar(morse) != '*') {
+            character += Character.toString(translator.morseToChar(morse));
+        }
+        text.setText(character);
+
+        // Now to try and write the whole output string
+        StringBuilder outputText = new StringBuilder("Mensagem: ");
+        for (char c : outputChars) {
+            outputText.append(c);
+        }
+        textNumber.setText(outputText.toString());
+    }
+
+    private void writeDash() {
+        String current = editText.getText().toString();
+        current += '-';
+        editText.setText(current);
+    }
+
+    private void writeDot() {
+        String current = editText.getText().toString();
+        current += '.';
+        editText.setText(current);
+    }
+
+    private void deleteMorse() {
+        String current = editText.getText().toString();
+        // If signal is not empty, delete last character
+        if (current.length() > 0) {
+            current = current.substring(0, current.length() - 1);
+            editText.setText(current);
+
+            // Otherwise, delete last character typed
+        } else if (outputChars.size() > 0) {
+            outputChars.removeLast();
+        }
+    }
+
+    private void endChar() {
+        String current = editText.getText().toString();
+        if (current.length() != 0) {
+            outputChars.add(translator.morseToChar(editText.getText().toString()));
+            editText.setText("");
+        } else {
+            outputChars.add(' ');
+        }
+    }
+
+    private void clearMsg() {
+        text.setText("Letra: ");
+        textWord.setText("Mensagem: ");
+        editText.setText("");
+        // clear() method of LinkedList empties it;
+        outputChars.clear();
+    }
 }
+
