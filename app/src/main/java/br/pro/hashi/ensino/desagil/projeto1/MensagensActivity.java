d40093b2debcc851ca6787926e51e3ad1f20328e
TheoBR00 <38351029+TheoBR00@users.noreply.github.com>
2020-05-09 10:19:48 -0300

    Colocando botões

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
@@ -3,14 +3,21 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import java.util.LinkedList;
 
 public class MensagensActivity extends AppCompatActivity {
+    private static Frases mensagens = new Frases();
+    private Button msg1;
+    private Button msg2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensagens);
+        msg1 = findViewById(R.id.msg_1);
+        msg2 = findViewById(R.id.msg_2);
     }
 
 
