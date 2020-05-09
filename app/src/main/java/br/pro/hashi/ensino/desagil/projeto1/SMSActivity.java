5b1bae1809885c09d07e61977723d4f50674a748
jpgianfaldoni <jpgianfaldoni@gmail.com>
2020-05-09 16:37:28 -0300

    teste

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -10,5 +10,6 @@ public class SMSActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_m_s);
+        //teste
     }
 }
