2d59e4c33a438ff17606afce15f48f19b021652a
CEDipEngineering <cadudip@hotmail.com>
2020-05-09 16:27:08 -0300

    Created activity.

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -0,0 +1,14 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SMSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_m_s);
+    }
+}
