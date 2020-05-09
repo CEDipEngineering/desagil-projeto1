3458c2c31a7690b36d80649cd2e459df4b639f44
TheoBR00 <38351029+TheoBR00@users.noreply.github.com>
2020-05-09 11:29:13 -0300

    ListView adicionada com mensagens

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
@@ -3,28 +3,46 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class MensagensActivity extends AppCompatActivity {
-    private static Frases mensagens = new Frases();
-    private Button msg1;
-    private Button msg2;
-    private Button msg3;
-    private Button msg4;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensagens);
-        msg1 = findViewById(R.id.msg_1);
-        msg2 = findViewById(R.id.msg_2);
-        msg3 = findViewById(R.id.msg_3);
-        msg4 = findViewById(R.id.msg_4);
+
+        listView=(ListView)findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Venha cá");
+        arrayList.add("Estou precisando de ajuda");
+        arrayList.add("É urgente!");
+        arrayList.add("Venha ver isso");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MensagensActivity.this, "clicked item:"+i+" "+arrayList.get(i).toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
-
-
 
 }
 
