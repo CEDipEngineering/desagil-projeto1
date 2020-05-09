8034f439b5dd17bbabf950d0d187883a7249e049
TheoBR00 <38351029+TheoBR00@users.noreply.github.com>
2020-05-09 09:54:03 -0300

    Implementando MensagensActivity

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
@@ -3,12 +3,17 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.util.LinkedList;
 
-public class Mensagens extends AppCompatActivity {
+public class MensagensActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensagens);
     }
+
+
+
 }
+
