363183ffb1a7fb1f600741ffd33ae9387f99251a
CEDipEngineering <cadudip@hotmail.com>
2020-05-10 09:56:14 -0300

    Functionality completed.

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,19 +19,8 @@ public class SMSActivity extends AppCompatActivity {
     private static final Translator translator = new Translator(); //protected quando tiverem mais activities?
     private final LinkedList<Character> outputChars = new LinkedList<>();
     private static final char[] numeros = new char[]{'0','1','2','3','4','5','6','7','8','9'};
-    private TextView text;
-    private TextView textWord;
-    private TextView textNumber;
-    private TextView textPhone;
-    private TextView editText;
-    private TextView textMessage;
-    private Button buttonSend;
-    private Button buttonMorse;
-    private Button buttonDelete;
-    private Button buttonEndChar;
-    private Button homeButton;
-    private Button SMSButton;
-    private Button backButton;
+    private TextView currentNumber, morseDisplay, messageDisplay, phoneDisplay;
+    private Button buttonBack, buttonMorse, buttonDelete, buttonEndChar, buttonSMS;
 
     // Método de conveniência para mostrar uma bolha de texto.
     private void showToast(String text) {
@@ -50,25 +38,26 @@ public class SMSActivity extends AppCompatActivity {
         setContentView(R.layout.activity_s_m_s);
 
         // Carregar os views
-        textMessage = findViewById(R.id.textWord);
-        textNumber = findViewById(R.id.textNumber);
-        buttonSend = findViewById(R.id.button_SMS);
-        backButton = findViewById(R.id.back_button);
-        text = findViewById(R.id.textLetter);
-        textWord = findViewById(R.id.textWord);
-        textNumber = findViewById(R.id.textNumber);
-        editText = findViewById(R.id.textNumber);
+        currentNumber = findViewById(R.id.current_number);
+        morseDisplay = findViewById(R.id.morse_display);
+        messageDisplay = findViewById(R.id.message_display);
+        phoneDisplay = findViewById(R.id.phone_display);
+
+        buttonBack = findViewById(R.id.button_back);
         buttonMorse = findViewById(R.id.button_morse);
         buttonDelete = findViewById(R.id.button_delete);
         buttonEndChar = findViewById(R.id.button_endChar);
+        buttonSMS = findViewById(R.id.button_SMS);
+
+
 
 
         // Usa a mensagem que foi enviada junto da criação do intent para mostrar o que será enviado
         Intent myIntent = getIntent();
         String message = myIntent.getStringExtra("message");
-        textMessage.setText("Mensagem: " + message);
+        messageDisplay.setText("Mensagem: " + message);
 
-        this.backButton.setOnClickListener((view) -> {
+        this.buttonBack.setOnClickListener((view) -> {
             Intent intent=new Intent(SMSActivity.this, MainActivity.class);
             intent.putExtra("message", message);
             startActivity(intent);
@@ -111,14 +100,13 @@ public class SMSActivity extends AppCompatActivity {
         });
 
         // Envia a mensagem
-        buttonSend.setOnClickListener((view) -> {
-
+        this.buttonSMS.setOnClickListener((view) -> {
             if (message.isEmpty()) {
                 showToast("Mensagem inválida!");
                 return;
             }
 
-            String phone = textPhone.getText().toString();
+            String phone = phoneDisplay.getText().toString();
 
             // Esta verificação do número de telefone é bem
             // rígida, pois exige até mesmo o código do país.
@@ -135,46 +123,46 @@ public class SMSActivity extends AppCompatActivity {
 
             // Limpar o campo para nenhum engraçadinho
             // ficar apertando o botão várias vezes.
-            textPhone.setText("");
+            phoneDisplay.setText("");
         });
     }
 
     private void update() {
 
         // Try to translate editText
-        String morse = editText.getText().toString();
+        String morse = morseDisplay.getText().toString();
         String character = "Número: ";
-        if ((translator.morseToChar(morse) != '*') && this.isNumber(morse)) {
+        if ((translator.morseToChar(morse) != '*' && this.isNumber(morse)) ) {
             character += Character.toString(translator.morseToChar(morse));
         }
-        text.setText(character);
+        currentNumber.setText(character);
 
         // Now to try and write the whole output string
         StringBuilder outputText = new StringBuilder("Telefone: ");
         for (char c : outputChars) {
             outputText.append(c);
         }
-        textNumber.setText(outputText.toString());
+        phoneDisplay.setText(outputText.toString());
     }
 
     private void writeDash() {
-        String current = editText.getText().toString();
+        String current = morseDisplay.getText().toString();
         current += '-';
-        editText.setText(current);
+        morseDisplay.setText(current);
     }
 
     private void writeDot() {
-        String current = editText.getText().toString();
+        String current = morseDisplay.getText().toString();
         current += '.';
-        editText.setText(current);
+        morseDisplay.setText(current);
     }
 
     private void deleteMorse() {
-        String current = editText.getText().toString();
+        String current = morseDisplay.getText().toString();
         // If signal is not empty, delete last character
         if (current.length() > 0) {
             current = current.substring(0, current.length() - 1);
-            editText.setText(current);
+            morseDisplay.setText(current);
 
             // Otherwise, delete last character typed
         } else if (outputChars.size() > 0) {
@@ -183,22 +171,22 @@ public class SMSActivity extends AppCompatActivity {
     }
 
     private void endChar() {
-        String current = editText.getText().toString();
+        String current = morseDisplay.getText().toString();
         if (current.length() != 0) {
-            char temp = translator.morseToChar(editText.getText().toString());
+            char temp = translator.morseToChar(current);
             if (this.isNumber(temp)) {
                 outputChars.add(temp);
             }
-            editText.setText("");
+            morseDisplay.setText("");
         } else {
             outputChars.add(' ');
         }
     }
 
     private void clearMsg() {
-        text.setText("Letra: ");
-        textWord.setText("Mensagem: ");
-        editText.setText("");
+        currentNumber.setText("Número: ");
+        phoneDisplay.setText("Telefone: ");
+        morseDisplay.setText("");
         // clear() method of LinkedList empties it;
         outputChars.clear();
     }
@@ -210,8 +198,12 @@ public class SMSActivity extends AppCompatActivity {
 
     //Overload
     private boolean isNumber(char c) {
-        boolean contains = Arrays.asList(numeros).contains(c);
-        return contains;
+        for (char n: numeros) {
+            if (c == n) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
