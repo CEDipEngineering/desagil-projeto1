48b1723aa712e3460c9c4582b7f2d7e27e7fe7e7
lucaskf1996 <email@example.com>
2020-05-11 17:52:13 -0300

    Read txt file from assets

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MensagensActivity.java
@@ -9,12 +9,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import java.io.InputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class MensagensActivity extends AppCompatActivity {
-    ListView listView;
+    private ListView listView;
+//    private Frases frases = new Frases();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +27,32 @@ public class MensagensActivity extends AppCompatActivity {
 
         ArrayList<String> arrayList = new ArrayList<>();
 
-        arrayList.add("Venha cá");
-        arrayList.add("Estou precisando de ajuda");
-        arrayList.add("É urgente!");
-        arrayList.add("Venha ver isso");
+        String text = "";
+        try{
+            InputStream inputStream = getAssets().open("prontas.txt");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            text = new String(buffer);
+            System.out.println(text);
+            String lines[] = text.split("\\r?\\n");
+            for (String line : lines) {
+                arrayList.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
 
         listView.setAdapter(arrayAdapter);
+//        System.out.println(arrayList);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(MensagensActivity.this, "clicked item:"+i+" "+arrayList.get(i).toString(), Toast.LENGTH_SHORT).show();
-
             }
         });
 
