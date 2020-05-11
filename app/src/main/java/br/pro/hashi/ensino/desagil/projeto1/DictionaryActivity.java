6b5d43fae659e7ebe7d598337474a32f3d1ced77
marcompp <marcomoliternop@gmail.com>
2020-05-11 13:38:42 -0300

    Layout do Dicionario Melhorado

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DictionaryActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DictionaryActivity.java
@@ -12,6 +12,9 @@ public class DictionaryActivity extends AppCompatActivity {
     private static final Dicionario dictionary = new Dicionario();
     private Button homeButton;
     private Button dicButton;
+    TextView textDicionario1;
+    TextView textDicionario2;
+    TextView textDicionario3;
 
 
     @Override
@@ -21,8 +24,12 @@ public class DictionaryActivity extends AppCompatActivity {
         homeButton = findViewById(R.id.home_button);
         dicButton = findViewById(R.id.troca_dic);
 
-        TextView textDicionario = findViewById(R.id.textDic);
-        textDicionario.setText(dictionary.getDicMorseChar());
+        textDicionario1 = findViewById(R.id.textDic1);
+        textDicionario2 = findViewById(R.id.textDic2);
+        textDicionario3 = findViewById(R.id.textDic3);
+        textDicionario1.setText(dictionary.getDicMorseChar1());
+        textDicionario2.setText(dictionary.getDicMorseChar2());
+        textDicionario3.setText(dictionary.getDicMorseChar3());
 
         this.homeButton.setOnClickListener((view) -> {
             Intent intent = new Intent(DictionaryActivity.this, HomeActivity.class);
@@ -31,10 +38,14 @@ public class DictionaryActivity extends AppCompatActivity {
 
         this.dicButton.setOnClickListener((view) -> {
             if (dictionary.isState()) {
-                textDicionario.setText(dictionary.getDicCharMorse());
+                textDicionario1.setText(dictionary.getDicCharMorse1());
+                textDicionario2.setText(dictionary.getDicCharMorse2());
+                textDicionario3.setText(dictionary.getDicCharMorse3());
                 dicButton.setText(".-    =>    A"); //Traduz de Morse
             } else {
-                textDicionario.setText(dictionary.getDicMorseChar());
+                textDicionario1.setText(dictionary.getDicMorseChar1());
+                textDicionario2.setText(dictionary.getDicMorseChar2());
+                textDicionario3.setText(dictionary.getDicMorseChar3());
                 dicButton.setText("A    =>    .-"); //Traduz para Morse
             }
         });
